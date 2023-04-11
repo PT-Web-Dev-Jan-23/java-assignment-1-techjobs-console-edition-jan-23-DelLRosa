ba42e77ce482855dc2137c008d25f512033437d5
Delaney <delaney.rosa1@gmail.com>
2023-04-11 07:51:20 -0500

    printJobs() fn now prints jobs.

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -119,6 +119,20 @@ public class TechJobs {
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
+        if (someJobs.size()==0){
+            System.out.println("No Results Found");
+            return;
+        }
+        for (Map<String,String>entry:someJobs){
+            System.out.println("\n*****");
+            for (String key: entry.keySet()) {
+                String value= entry.get(key);
+                System.out.println(key+": "+value);
+            }
+            System.out.println("*****\n");
+
+        }
+
 
         System.out.println("printJobs is not implemented yet");
     }
